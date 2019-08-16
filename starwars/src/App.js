548ec760ca34022093107ced3458563c0281ff7a
daniela-arias-ordonez <daniela.arias2000@gmail.com>
2019-08-16 12:33:13 -0500

    Added CharacterCards component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+export default App;
+
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import CharacterCard from "./components/CharacterCard.js";
+import { Container } from 'semantic-ui-react';
 
 const App = () => {
+  const App = () => {
+  const[swCharacter, setSwCharacter] = useState ([]);
+
+  useEffect(() => {
+  axios
+  .get("https://swapi.co/api/people/")
+  .then(res => { 
+  setSwCharacter(res.data.results);
+  })
+.catch (err => {
+  console.log(err);
+})
+  }, []);
+
+console.log("test", swCharacter);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,8 +32,8 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Container className="container-cards">
+      <CharacterCard characters={swCharacter}/>
+      </Container>
     </div>
   );
-}
-
-export default App;
